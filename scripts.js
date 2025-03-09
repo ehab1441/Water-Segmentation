05bf30bae4056b56e5b19242d6df4a5ebb366118
Mohamed Ehab <104400937+ehab1441@users.noreply.github.com>
2025-03-09 02:47:58 +0200

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-function showInfo() {
-    const infoBox = document.getElementById("info-box");
-    if (infoBox.style.display === "none" || infoBox.style.display === "") {
-        infoBox.style.display = "block";  // Show the info box
-    } else {
-        infoBox.style.display = "none";  // Hide the info box
-    }
-}
-
